0b9164294cdffa483fe49cd796e6ca20f133d3de
Bruno Francioni <brunofrancioni1998@gmail.com>
2021-11-13 11:52:44 -0300

    Environment variables changes

--- src/core/services/CharactersServices.ts
+++ src/core/services/CharactersServices.ts
@@ -2,11 +2,11 @@ import API from "../utils/API/API";
 
 class CharactersServices {
     public async getCharacters(start: number, limit: number) {
-        return await API.get(`/characters?ts=1000&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}&offset=${start}&limit=${limit}`);
+        return await API.get(`/characters?ts=1000&apikey=${String(process.env.REACT_APP_API_KEY)}&hash=${process.env.REACT_APP_HASH}&offset=${start}&limit=${limit}`);
     }
 
     public async getCharacter(id: string) {
-        return await API.get(`/characters/${id}?ts=1000&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`);
+        return await API.get(`/characters/${id}?ts=1000&apikey=${String(process.env.REACT_APP_API_KEY)}&hash=${process.env.REACT_APP_HASH}`);
     }
 }
 
--- src/core/utils/firebase/firebase.ts
+++ src/core/utils/firebase/firebase.ts
@@ -8,13 +8,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: String(process.env.REACT_APP_FIREBASE_API_KEY),
+  authDomain: String(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+  projectId: String(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+  storageBucket: String(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.REACT_APP_FIREBASE_APP_ID),
+  measurementId: String(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID)
 };
 
 // Initialize Firebase
